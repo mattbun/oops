6f3015e3a16c258b2efc2f136e154ab918072193
Matt Rathbun <5514636+mattbun@users.noreply.github.com>
2020-10-24 16:25:26 -0400

    Make .oops directory if it doesn't exist in config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 
@@ -5,3 +6,6 @@ import * as path from 'path';
 export const baseDirectory = path.join(os.homedir(), '.oops');
 export const dbLocation = path.join(baseDirectory, 'db.json');
 export const stashesDirectory = path.join(baseDirectory, 'stashes');
+
+// TODO put this somewhere better
+fs.mkdirSync(baseDirectory, { recursive: true });
